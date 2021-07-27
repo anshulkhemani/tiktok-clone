8a63a9ede3fd320d70fa1dab693e1cc8a7582e96
anshulkhemani <anshulkkhemani@gmail.com>
2021-07-27 10:07:38 +0530

    Backend

---
+++ tiktok-backend/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        url: "https://stream.mux.com/m62Cveh9Q7G7OIFgiv1JdjYRJydvp8RN/high.mp4?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJleHAiOjE2MjcyMDcyMTksImF1ZCI6InYiLCJzdWIiOiJtNjJDdmVoOVE3RzdPSUZnaXYxSmRqWVJKeWR2cDhSTiJ9.JxOa5iTiaEn3cN1j1j9l8wxJUsGS2ERj7FvN2lxNvysyx34x3kfXhgqe7HXN2VjWWIZG3Xv3aOlQ5BX6HCQ7HBHPGQhKNRRjznJTvx3Z8rzO6xR5eiN-q5oiF9RFCoy2LTL4TEcwx8-1qO1OQhHGvSUOrf4ohncKmwAiVcV9DOsOsuphyHHlscA-Nv6Re4jh1vPcz3T3ApaxSfht7RAmCIaWzAlZROzr6Y--ktTwdc2Whky8hU7AD_dqAryY1zYVggJaHrhp326aupaT4iq3kmjvXT1C59beaXfGgjnzEDu8uV3FWLmuTMhVDHU_qksL_mFsPfHVcIRQX6ANxs81tg",
+        channel:"ashkhem",
+        description:"this is a sample video",
+        song:"this is a song",
+        likes:"434",
+        messages:"128",
+        shares:"13"
+    }
+]
+
+export default data
